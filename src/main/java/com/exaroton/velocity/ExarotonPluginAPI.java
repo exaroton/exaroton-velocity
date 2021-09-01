44abd322e98c6c0d28bf418b151b77832c57627f
Julian <julian@aternos.org>
2021-09-01 14:45:38 +0200

    public plugin api

---
+++ src/main/java/com/exaroton/velocity/ExarotonPluginAPI.java
@@ -0,0 +1,148 @@
+package com.exaroton.velocity;
+
+import com.exaroton.api.APIException;
+import com.exaroton.api.server.Server;
+import com.exaroton.api.server.ServerStatus;
+import com.velocitypowered.api.proxy.server.RegisteredServer;
+import com.velocitypowered.api.proxy.server.ServerInfo;
+
+import java.util.Optional;
+
+public class ExarotonPluginAPI {
+
+    private static ExarotonPlugin plugin;
+
+    /**
+     * find a server by it's
+     * - proxy name
+     * - id
+     * - address (e.g. example.exaroton.me)
+     * - server name
+     * @param query search query
+     * @return found server
+     * @throws APIException exception while fetching server list
+     */
+    public static Server findServer(String query) throws APIException {
+        return plugin.findServer(query, true);
+    }
+
+    /**
+     * start a server and add it to the proxy
+     * @param server server to start
+     * @return started successfully
+     * @throws APIException exception while starting server
+     */
+    public static boolean startServer(Server server) throws APIException {
+        if (server == null) {
+            throw new NullPointerException("No server provided!");
+        }
+
+        if (!server.hasStatus(new int[]{ServerStatus.OFFLINE, ServerStatus.CRASHED})) {
+            return false;
+        }
+
+        watchServer(server);
+        server.start();
+        return true;
+    }
+
+    /**
+     * stop a server and remove it from the proxy
+     * the server will automatically be added again when it goes online again
+     * @param server server to stop
+     * @return stopped successfully
+     * @throws APIException exception while stopping server
+     */
+    public static boolean stopServer(Server server) throws APIException {
+        if (server == null) {
+            throw new NullPointerException("No server provided!");
+        }
+
+        if (!server.hasStatus(ServerStatus.ONLINE)) {
+            return false;
+        }
+
+        watchServer(server);
+        server.stop();
+        return true;
+    }
+
+    /**
+     * restart a server and add it to the proxy
+     * @param server server to restart
+     * @return restarted successfully
+     * @throws APIException exception while restarting server
+     */
+    public static boolean restartServer(Server server) throws APIException {
+        if (server == null) {
+            throw new NullPointerException("No server provided!");
+        }
+
+        if (!server.hasStatus(ServerStatus.ONLINE)) {
+            return false;
+        }
+
+        watchServer(server);
+        server.restart();
+        return true;
+    }
+
+    /**
+     * watch status updates for this server and add it to the proxy if it's currently online
+     * @param server server to add
+     * @return was it added
+     */
+    public static boolean addServer(Server server) {
+        if (server == null) {
+            throw new NullPointerException("No server provided!");
+        }
+
+        ServerStatusListener listener = watchServer(server);
+        String name = listener.getName(server);
+
+        if (server.hasStatus(ServerStatus.ONLINE)) {
+            if (plugin.getProxy().getServer(name).isPresent()) {
+                return false;
+            }
+            else {
+                plugin.getProxy().registerServer(plugin.constructServerInfo(name, server));
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * watch this server
+     * add it the proxy when it becomes online
+     * remove it when it goes offline
+     * @param server server to watch
+     */
+    public static ServerStatusListener watchServer(Server server) {
+        if (server == null) {
+            throw new NullPointerException("No server provided!");
+        }
+
+        String name = plugin.findServerName(server.getAddress());
+        Optional<RegisteredServer> registeredServer = plugin.getProxy().getServer(name);
+
+        return plugin.listenToStatus(server, registeredServer.map(RegisteredServer::getServerInfo).orElse(null), name);
+    }
+
+    /**
+     * stop watching updates for this server
+     * @param server server
+     */
+    public static void stopWatchingServer(Server server) {
+        if (server == null) {
+            throw new NullPointerException("No server provided!");
+        }
+
+        plugin.stopListeningToStatus(server.getId());
+    }
+
+    static void setPlugin(ExarotonPlugin plugin) {
+        ExarotonPluginAPI.plugin = plugin;
+    }
+}
+
